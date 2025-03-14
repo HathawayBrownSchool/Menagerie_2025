f79e8f97b0517c6126aa5852f6dffe493e27a603
asu2026 <158514513+asu2026@users.noreply.github.com>
2025-03-14 11:51:29 -0400

    Add files via upload

--- Monkey.java
+++ Monkey.java
@@ -1,4 +1,6 @@
 public class Monkey extends Animal{
+    EasySound voice = new EasySound("mixkit-cartoon-monkey-applause-103.wav");
+    
     public Monkey(String m)
     {
         super(m);
@@ -7,5 +9,12 @@ public class Monkey extends Animal{
     @Override
     public String eat(){
         return "Banannaanna";
+    }
+    
+    @Override
+    public String speak()
+    {
+        voice.play();
+        return "oopoh aaaah";
     }
 }
